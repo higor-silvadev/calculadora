814bd3408673bbc9ff9c9af81d75dd75d9178c05
higor-silvadev <151585174+higor-silvadev@users.noreply.github.com>
2025-12-10 02:05:36 -0300

    feat: update Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
 import './Button.css'
 
-const Button = ({ label }) => (
-    <button className="button">{label}</button>
-)
+export default function Button({ label, click, operation, double, triple }) {
+    let classes = 'button '
+    classes += operation ? 'operation ' : ''
+    classes += double ? 'double ' : ''
+    classes += triple ? 'triple ' : ''
 
-export default Button
+    return (
+        <button
+            className={classes}
+            onClick={e => click && click(e.target.innerHTML)}
+        >
+            {label}
+        </button>
+    )
+}
